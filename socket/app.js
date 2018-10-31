cbf596e595c6c30e3f319e1e2fb7a64cfca3aa67
lronelove <657828543@qq.com>
2018-10-31 10:47:22 +0800

    实现了线程池下面的redis存储

--- socket/app.js
+++ socket/app.js
@@ -1,14 +1,13 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const Redis = require('ioredis')
 
 const config = require('./config/index.js') // 配置信息
 const createRedisServer = require('./redis/index.js') // 创建redis服务的函数
 const SocketHandler = require('./socketHandler/index.js') // 处理socket事件对应的方法
 const HttpHandler = require('./httpHandler/index.js') // 处理http请求对应的方法
 
-const redis = createRedisServer(Redis, config.redis.host, config.redis.port) // 创建redis服务
+const redis = createRedisServer() // 创建redis服务
 
 // socket 链接
 io.on('connection', (socket) => {
