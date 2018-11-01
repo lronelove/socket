a987108ded4372eb829c4c98d511e143dc077c55
lronelove <657828543@qq.com>
2018-11-01 10:25:28 +0800

    这是稳定得版本

--- socket/httpHandler/index.js
+++ socket/httpHandler/index.js
@@ -2,17 +2,17 @@ class HttpHandler {
 	// 测试的方法，可以推送socket服务，而且也可以发送ajax请求
 	static async test (redis, io, req, res) {
 		let socketId = await redis.get('lronelove').then(res => {
-      return res
-  	})
-  	let target = io.sockets.connected[socketId]
+       		return res
+  		})
+  		let target = io.sockets.connected[socketId]
   		
-  	if (target) {
-    	target.emit('message', 'I am from ajax')
-  	}
-  	res.send({
-    	code: 0,
-    	data: socketId
-  	})
+  		if (target) {
+    		target.emit('message', 'I am from ajax')
+  		}
+  		res.send({
+    		code: 0,
+    		data: socketId
+  		})
 	}
 }
 
